48dc4b7c5768186459182d9c26ce1d643a887289
Vivek.R.Nair <vivekrnair2004@gmail.com>
2025-12-03 06:23:57 +0530

    my third  commit

--- gitnotes.sh
+++ gitnotes.sh
@@ -5,5 +5,12 @@ git.status to check status of file
 git.add to tell git whether u should keep an eye on it
 git coomit to save the snapshot
 git remote add origin <repo url> to connect to github
- just learning
+we can use git checkout to switch between branches if we use -b it will create a new branch 
+eg: git checkout -b learning creates a new branch named learning
+and we will be doing the works in branch now 
+
+for merging we csn use either cherrypick,merge, or rebase 
+	cherry pick is easy but diidficult for large projects as we need to  chooose the commit id 
+ if we use merge it will be pushed on to top so hard for linear loggging
+if we use rebasse it folows linear looging
 
