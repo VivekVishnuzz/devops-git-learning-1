2393a7cea0a59c4b81fb5ea25043d049c8fdd590
Vivek.R.Nair <vivekrnair2004@gmail.com>
2025-12-06 06:32:30 +0530

    note containing aws seervices for devops engineers

---
+++ aws services fordevops.sh
@@ -0,0 +1,18 @@
+
+these are the most required services a devops engineer should know
+
+1.ec2
+2.vpc
+3.ebs
+4.s3
+5.iam 
+6.cloud watch
+7.lamda
+8.cloud build services  
+9.aws configuration services
+10.billing and costing
+11.aws kms
+12.cloud trail
+13.aws eks very important
+14.fargate/ecs
+15.elk
